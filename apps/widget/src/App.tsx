a6030d933bbecfd967552d57442eec2f9ac8e1b6
reanru <runtufandi@gmail.com>
2025-05-24 20:22:16 +0800

    setup visitor socket ini session

--- apps/widget/src/App.tsx
+++ apps/widget/src/App.tsx
@@ -1,10 +1,46 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { v4 as uuidv4 } from 'uuid';
+import {io, Socket} from 'socket.io-client';
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [socket, setSocket] = useState<Socket | null>(null);
+
+  useEffect(() => {
+    // Menghubungkan ke server Socket.IO
+    const newSocket = io('http://localhost:3001');                
+    setSocket(newSocket);     
+
+    const visitor = localStorage.getItem("visitor");
+
+    if(!visitor) localStorage.setItem("visitor", uuidv4());
+    // else newSocket.emit('init-session', visitor);
+    
+    newSocket.emit('init-session', visitor);
+
+    return () => {
+        console.log('disconnect');
+        newSocket.disconnect();    
+    }
+  }, []);
+
+  useEffect(() => {
+    if(socket){
+      socket.on('receiveMessage', (data) => {
+          // console.log(`cek receiveMessage (${selectedFriend} - ${userId}) `);
+          // if(data.sender === selectedFriend && data.receiver === userId){
+          //     setMessages(prevState => {
+          //         return [...prevState, data]
+          //     })
+          // }
+          // setNewMessage(data);
+      });
+  }
+  }, [socket])
+  
 
   return (
     <>
@@ -16,18 +52,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
+      <form className="max-w-sm mx-auto">
+        <div className="mb-5">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+          <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+        </div>
+        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+      </form>
+
+
     </>
   )
 }
--- server/socket-server/src/application/socket-io.ts
+++ server/socket-server/src/application/socket-io.ts
@@ -1,13 +1,65 @@
 import { web } from './web';
 import { Server } from 'socket.io';
 import { createServer } from 'http';
-
+import { prismaClient } from "../application/database";
 const server = createServer(web);
 
 const io = new Server(server, {
     cors : {
         origin: "*",
     }
+});
+
+// Event ketika klien terhubung ke server Socket.IO
+io.on('connection', (socket) => {
+
+    let availableAgents: string[] = []; // ['agentId']
+    let availableRooms = []; // { room: 'visitorId', agent: 'agentId' }
+
+    console.log(`User ${socket.id} connected`);
+
+    socket.on('init-session', async (data) => {
+        const checkSession = await prismaClient.session.findFirst({
+            where: {
+                visitorId: data
+            }
+        });
+
+        if(!checkSession){
+            await prismaClient.session.create({
+                data: {
+                    visitorId: data,
+                    isActive: true,
+                    assignedTo: availableAgents[0] ?? null
+                },
+            });
+
+        }
+        
+        socket.join(data);
+        // io.to(data).emit('receiveMessage', data);
+    });
+
+    // Menangani permintaan masuk room untuk visitor
+    socket.on('joinRoomVisitor', (room) => {
+        console.log(`Visitor ${socket.id} joined rooms: ${room}`);
+    });
+
+    // Menangani permintaan masuk room untuk agant
+    socket.on('joinRoomAgent', (room) => {
+        // socket.join(room);
+
+        console.log(`Agent ${socket.id} joined rooms: ${room}`);
+    });
+
+    // Menangani pesan dari klien untuk kirim data ke orang lain
+    socket.on('sendMessage', (data) => {
+        // Mengirim pesan ke semua klien dalam room yang sama
+
+        console.log(`User send to room : ${data.receiver}#${data.sender} - message : ${data.message}`);
+
+        // io.to(`${data.receiver}#${data.sender}`).emit('receiveMessage', data);
+    });
 });
 
 export default server;
