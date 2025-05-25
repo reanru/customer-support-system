7ac1e01e0a6b6f6d7dfd44d1de3804cd22d05330
reanru <runtufandi@gmail.com>
2025-05-25 23:21:02 +0800

    update UI form chat widget

--- apps/widget/src/App.tsx
+++ apps/widget/src/App.tsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 import { v4 as uuidv4 } from 'uuid';
 import {io, Socket} from 'socket.io-client';
 
 function App() {
   const [socket, setSocket] = useState<Socket | null>(null);
+  const [message, setMessage] = useState("");
+
+  const [visitorId, setVisitorId] = useState("");
 
   useEffect(() => {
     // Menghubungkan ke server Socket.IO
@@ -21,7 +23,8 @@ function App() {
       localStorage.setItem("visitor", uuid);
       visitor = uuid;
     }
-    
+    setVisitorId(visitor);
+
     newSocket.emit('join-room-visitor', visitor);
 
     return () => {
@@ -44,26 +47,44 @@ function App() {
   }
   }, [socket])
   
+  const sendMessage = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      // socket?.emit('send-message', { 
+      //   sender: userId, 
+      //   receiver: selectedFriend, 
+      //   message: input 
+      // });
+
+      setMessage('');
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <div className="bg-gray-100 flex justify-center items-center h-screen">
 
+        <div className="w-full max-w-8/12 lg:max-w-4/12 space-y-1 bg-white p-4 rounded-2xl">
 
-      <form className="max-w-sm mx-auto">
-        <div className="mb-5">
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-          <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+          <div className="flex flex-col justify-end w-full space-y-1 p-2 h-96 border border-gray-300 rounded-lg">
+
+            <div className="bg-gray-100 p-2 text-xs mr-auto max-w-8/12">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+            <div className="bg-blue-100 p-2 text-xs ml-auto max-w-8/12">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+
+          </div>
+
+          <form className="flex w-full items-center">
+            <div className="flex w-full space-x-1">
+              <input 
+                type="text" 
+                className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5" placeholder="name@flowbite.com" required
+                onChange={(e) => setMessage(e.target.value)} 
+              />
+              <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Submit</button>
+            </div>
+          </form>
         </div>
-        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-      </form>
+        </div>
+
 
 
     </>
